4ade114ec8ba1f2ca17bf525102edafd7a034a2c
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-09 21:57:47 +0530

    created sidebar.js

---
+++ whatissup/components/Sidebar.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+function Sidebar() {
+    return (
+        <Container>
+            <h1>sidebar</h1>
+        </Container>
+    )
+}
+
+export default Sidebar
+
+
+const Container = styled.div `
+ 
+`
