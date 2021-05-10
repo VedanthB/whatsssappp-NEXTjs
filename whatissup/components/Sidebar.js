3a0644f3415b819f012924ebeecbdf1e436bc284
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-10 20:16:51 +0530

    sidebar => header + search

--- whatissup/components/Sidebar.js
+++ whatissup/components/Sidebar.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 import Avatar from '@material-ui/core/Avatar';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChatIcon from '@material-ui/icons/Chat';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
 
 
 function Sidebar() {
@@ -7,7 +11,22 @@ function Sidebar() {
         <Container>
             <Header>
                 <UserAvatar />
+
+                <IconContainer>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </IconContainer>
             </Header>
+
+            <Search>
+                <SearchIcon />
+                <SearchInput placeholder='search chats....' />
+            </Search>
         </Container>
     )
 }
@@ -20,9 +39,40 @@ const Container = styled.div `
 `
 
 const Header = styled.div `
- 
+   display: flex;
+   position: sticky;
+   top: 0;
+   background-color: white;
+   z-index: 1;
+   justify-content: space-between;
+   align-items: center;
+   padding: 15px;
+   height: 80px;
+   border-bottom: 3px solid whitesmoke;
 `
 
 const UserAvatar = styled(Avatar)  `
-  margin: 10px;
+   cursor: pointer;
+
+   :hover {
+       opacity: 0.8;
+   }
+`
+
+const IconContainer = styled.div ` 
+ 
+`
+
+const Search = styled.div `
+ display: flex;
+ align-items: center;
+ padding: 20px;
+ border-radius: 2px;
+`
+
+const SearchInput = styled.input `
+ outline-width: 0;
+ border: none;
+ flex: 1;
+
 `
