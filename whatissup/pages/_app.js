39e36f2cd0687854c599caba1f13006b18d2637d
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-14 07:38:13 +0530

    capturing and storing user data

--- whatissup/pages/_app.js
+++ whatissup/pages/_app.js
@@ -7,6 +7,22 @@ import Loading from '../components/Loading'
  function App({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth) 
 
+  // this is to get the user info after logged in so we can use it later in the app
+  useEffect(() => {
+    if (user) {
+      db.collection("users").doc(user.uid).set(
+        {
+          email: user.email,
+          name: user.displayName,
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      );
+    }
+  }, [user]);
+
+
   if (loading) return <Loading />
   if (!user) return <Login />
   return <Component {...pageProps} />
