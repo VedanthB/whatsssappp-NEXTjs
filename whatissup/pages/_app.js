5a25845629ae2ccee290e5a4aa6b560ce5ab1582
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-14 07:29:02 +0530

    ADD: Loading component and loading from firebase hooks useAuthState

--- whatissup/pages/_app.js
+++ whatissup/pages/_app.js
@@ -1,11 +1,13 @@
 import { auth, db } from '../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import Login from './login'
+import Loading from '../components/Loading'
 
 
  function App({ Component, pageProps }) {
-  const [user] = useAuthState(auth) 
+  const [user, loading] = useAuthState(auth) 
 
+  if (loading) return <Loading />
   if (!user) return <Login />
   return <Component {...pageProps} />
   
