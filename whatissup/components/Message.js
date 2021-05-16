6f0eebe74463c1c1bb70f12003b2d28da8e3be27
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-16 22:13:41 +0530

    ADD: messages styling and moment for timestAMP

--- whatissup/components/Message.js
+++ whatissup/components/Message.js
@@ -1,16 +1,62 @@
-import styled from 'styled-components'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import styled from "styled-components";
+import moment from "moment";
 
-function Message({user, message}) {
-    return (
-       <Container>
-           <p> {message.message} </p>
-       </Container>
-    )
+function Message({ user, message }) {
+  const [userLoggedIn] = useAuthState(auth);
+
+  const TypeOfMessage = user === userLoggedIn.email ? Sender : Reciever;
+
+  return (
+    <Container>
+      <TypeOfMessage>
+        {message.message}
+
+
+        <Timestamp>
+          {message.timestamp ? moment(message.timestamp).format("LT") : "..."}
+        </Timestamp>
+      </TypeOfMessage>
+    </Container>
+  );
 }
 
-export default Message
+export default Message;
 
+const Container = styled.div``;
+const MessageElement = styled.p`
+  width: fit-content;
+  padding: 15px;
+  border-radius: 8px;
+  margin: 10px;
+  min-width: 60px;
+  padding-bottom: 26px;
+  position: relative;
+  text-align: right;
+`;
 
-const Container = styled.div `
- color: white;
-`
+const Sender = styled(MessageElement)`
+  margin-left: auto;
+  background-color: #056162;
+  word-break: break-word;
+  color: lightGray;
+`;
+
+const Reciever = styled(MessageElement)`
+  background-color: #262d31;
+  text-align: left;
+  word-break: break-word;
+  color: lightGray;
+`;
+
+const Timestamp = styled.span`
+  color: gray;
+  font-size: 9px;
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 0;
+  padding-bottom: 10px;
+  padding-right: 5px;
+`;
